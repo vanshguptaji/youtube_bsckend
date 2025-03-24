9b5701a8d34df7e58aaa690c12607512067aceb9
Vansh Gupta <vanshg1122334455@gmai.com>
2025-03-24 12:28:13 +0530

    updated controller and models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {ApiError} from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
@@ -18,25 +18,27 @@ const registerUser = asyncHandler(async (req, res, next) =>{
     // return response
 
 
-    const {fullName, email, username, password } = req.body
+    const { fullName, email, username, password } = req.body
     console.log("email :", email);
 
     // if(fullName === "") {
     //     throw new ApiError(400, "Fullname is required")
     // }
     if(
-        [fullName, emai, username, password].some((field) => field?.trim() === "")
+        [fullName, email, username, password ].some((field) => field?.trim() === "")
     ){
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUSer = User.findOne({
+    const existedUSer = await User.findOne({
         $or: [{ username } , { email }]
     })
 
     if(existedUSer){
         throw new ApiError(409, "User with email or username already exists")
     }
+     
+    console.log("req.files.avatar :", req.files.avatar);
     
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath = req.files?.coverImage[0]?.path;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       trim: true,
       lowercase: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -67,7 +67,7 @@ userSchema.methods.generateAccessToken = function () {
       _id: this._id,
       email: this.email,
       username: this.username,
-      fullName: this.fullname,
+      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
